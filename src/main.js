6998510e7dd968ce5ddd4dac99605dda1c673b69
wrunion <winterrunion@gmail.com>
2020-03-21 11:28:47 -0700

    UI complete. All accordion divs working.

--- src/main.js
+++ src/main.js
@@ -5,48 +5,29 @@ import $ from 'jquery';
 
 $(document).ready(function() {
 
-  // $(".clickable-p").click(function(){
-  //   const id = this.id;
-  //   $(`#id-input-div`).show();
-  //   console.log(id);
-  // }); 
-
-  // $(".clickable-p").click(function(e){
-  //   const id = this.id;
-  //   const divString = `${id}-input-div`;
-  //   $(divString).show();
-  // });
-
-  // $(".clickable-p").click(function(e){
-  //   console.log(e.target.id);
-  //   const id = this.id;
-  //   const divToShow = document.getElementById(id);
-  //   const displayString = `${divToShow}-input-div`;
-  //   $(`${displayString}`).show();
-  // });
-
-  /* HOW TO REFACTOR????? */
+  /* How do I refactor this into a single function? */
   $("#bike").click(function() {
+    $(".accordion-item").hide();
     $("#bike-input-div").toggle();
   });
-  // This isn't working. Don't know why.
   $("#weather").click(function() {
+    $(".accordion-item").hide();
     $("#weather-input-div").toggle();
   });
   $("#photo").click(function() {
+    $(".accordion-item").hide();
     $("#photo-input-div").toggle();
   });
   $("#news").click(function() {
+    $(".accordion-item").hide();
     $("#news-input-div").toggle();
   });
   $("#cat").click(function() {
+    $(".accordion-item").hide();
     $("#cat-input-div").toggle();
   });
 
 
-  // $(".clickable-p").click(function() {
-  //   const id = `"#this.id-input-div"`;
-  //   $(id).show();
-  // });
+
 
 });
