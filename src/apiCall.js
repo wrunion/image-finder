b022b8a4c487223136d79ffc719055815adb0af5
wrunion <winterrunion@gmail.com>
2020-03-25 01:26:36 -0700

    shift direction toward Unsplash image app. Update files accordingly.

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,88 +1,54 @@
 /* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import './styles.css';
+import $ from 'jquery';
 import './../.env';
 
-export async function makeApiCall(searchTerm) {
-  console.log(searchTerm);
-  try {
-    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.API_KEY}
-    `);
-    let parsedResponse;
-    if (response.ok && response.status === 200) {
-      parsedResponse = await response.json();
-    } else {
-      parsedResponse = false;
-    }
-
-    showResults(parsedResponse);
-    
-  } catch(e) {
-    showResults(false);
-    console.log(e.message);
-  }
+function capitalizeString(str) {
+  return str.charAt(0).toUpperCase() + str.substring(1);
 }
-const showResults = function(results) {
-  if (results && results.data && results.data[0]) {
-    let firstResult = results.data[0];
-    console.log(firstResult);
-    console.log(results);
-    // console.log(results.results);
-    return results;
-  } else {
-    console.log(`There are no doctors that meet that criteria. Please try again.`);
-  }
-};
 
-//   const showPhoto = function(data) {
-//     if (data && data.results && data.results[0]) {
-//       const firstImage = data.results[0];
-//       const url = firstImage.urls.regular;
-//       const author = firstImage.user.name;
-//       const alt_description = firstImage.alt_description;
+/* REFERENCE: https://unsplash.com/developers */
 
-//       let htmlContent = `<figure>
-//         <img src="${firstImage.urls.regular}" alt="${firstImage.alt_description}" class="img-fluid">
-//         <figcaption>${displayKeyword} by ${firstImage.user.name}</figcaption>
-//       </figure>`;
-//       $("#results").append(htmlContent);
-//     } else {
-//       $("#results").append(`There was an error handling your request. Please check your inputs and try again!`);
-//     }
-//   }; 
-//   console.log(searchTerm);
-// }
+$(document).ready(function() {
 
+  $(".clickable-p").click(function() {
+    $('.accordion-item').hide();
+    $(this).siblings("div").show();
+  });
 
-/* ENDPOINTS */
-/* List of specialties (see sample-specialties.json): 
-https://api.betterdoctor.com/2016-03-01/specialties?user_key=f820b6b9b53355ead49a199b427f18ad */
+  $("#keyword-button").click(function() {
+    $('#results').empty();
+    const keyword = $("#keyword-input").val().toLowerCase();
+    const displayKeyword = capitalizeString(keyword);
+    (async () => {
+      try {
+        let response = await fetch(`https://api.unsplash.com/search/photos?query=${keyword}&client_id=${unsplashAccessKey}`);
+        let parsedResponse;
+        if (response.ok && response.status === 200) {
+          parsedResponse = await response.json();
+        } else {
+          parsedResponse = false;
+        }
+        showPhoto(parsedResponse);
+      } catch(e) {
+        showPhoto(false);
+        console.log(e.message);
+      }
+    })();
 
-/* List of conditions (see sample-conditions.json): 
-https://api.betterdoctor.com/2016-03-01/conditions?user_key=f820b6b9b53355ead49a199b427f18ad */
+    const showPhoto = function(data) {
+      if (data && data.results && data.results[0]) {
+        const firstImage = data.results[0];
 
-/* List of providers: 
- var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=f820b6b9b53355ead49a199b427f18ad */
-
- /* PROVIDERS WHOOOOOOOO */
- /* https://api.betterdoctor.com/2016-03-01/doctors?last_name=Smith&query=toothache&skip=0&limit=10&user_key=f820b6b9b53355ead49a199b427f18ad 
- Can search by:
- -name (partial accepted)
- -first_name
- -last_name
- -query (ie "toothache") - searches all fields by a keyword query
- -specialty_uid
- -location
- -user_location
- -gender
- -skip (how many items to skip ahead)
- -limit (maximum num of items to receive)
-
- Sample Query:
- https://api.betterdoctor.com/2016-03-01/doctors?last_name=Smith&query=toothache&skip=0&limit=10&user_key=f820b6b9b53355ead49a199b427f18ad
-
- Sample Query 2:
- https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=f820b6b9b53355ead49a199b427f18ad
- */
-
-/* GET A SPECIFIC DOCTOR BY UID /*
-
-/* https://api.betterdoctor.com/2016-03-01/doctors/12345?user_key=f820b6b9b53355ead49a199b427f18ad */
+        let htmlContent = `<figure>
+          <img src="${firstImage.urls.regular}" alt="${firstImage.alt_description}" class="img-fluid">
+          <figcaption>${displayKeyword} by ${firstImage.user.name}</figcaption>
+        </figure>`;
+       
+        $("#results").append(htmlContent);
+      } else {
+        $("#results").append(`There was an error handling your request. Please check your inputs and try again!`);
+      }
+    }; 
+  });
--- src/main.js
+++ src/main.js
@@ -1,29 +1,96 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */ 
 import './styles.css';
 import $ from 'jquery';
 import './../.env';
-import { makeApiCall } from './apiCall.js';
 
-console.log(process.env.API_KEY);
+const unsplashAccessKey = "YOUR_UNSPLASH_ACCESS_KEY_HERE";
+
+function capitalizeString(str) {
+  return str.charAt(0).toUpperCase() + str.substring(1);
+}
+
+function generateRandomNumber() {
+  return Math.floor(Math.random()*10+1);
+}
+
+/* REFERENCE: https://unsplash.com/developers */
 
 $(document).ready(function() {
-  $('form').submit(function() {
-    const symptom = $('#primary-symptom-input').val().toLowerCase();
-    console.log(symptom);
 
-    makeApiCall(symptom);
-    
-    const outputSpan = $('span#symptom-span');
-    outputSpan.append(symptom);
-    event.preventDefault();
+  $(".clickable-p").click(function() {
+    $('.accordion-item').hide();
+    $(this).siblings("div").show();
+  });
+
+  $("#keyword-button").click(function() {
+    $('#results').empty();
+    const keyword = $("#keyword-input").val().toLowerCase();
+    const displayKeyword = capitalizeString(keyword);
+    (async () => {
+      try {
+        let response = await fetch(`https://api.unsplash.com/search/photos?query=${keyword}&client_id=${process.env.API_KEY}`);
+        let parsedResponse;
+        if (response.ok && response.status === 200) {
+          parsedResponse = await response.json();
+        } else {
+          parsedResponse = false;
+        }
+        showPhoto(parsedResponse);
+      } catch(e) {
+        showPhoto(false);
+        $("#results").append(`There was an error processing your request.`);
+        console.log(e.message);
+      }
+    })();
+
+    const showPhoto = function(data) {
+      if (data && data.results && data.results[0]) {
+        /* Generate a random number, so that a user searching the same term multiple times can see a variety of outputs */
+        const imageNum = generateRandomNumber();
+        const image = data.results[imageNum];
+
+        let htmlContent = `<figure>
+          <img src="${image.urls.regular}" alt="${firstImage.alt_description}" class="img-fluid">
+          <figcaption>${displayKeyword} by ${firstImage.user.name}</figcaption>
+        </figure>`;
+       
+        $("#results").append(htmlContent);
+      } else {
+        $("#results").append(`No results found. Please check your inputs and try again!`);
+      }
+    }; 
+  });
+
+
+  // (async () => {
+  //   try {
+  //     let response = await fetch(`https://api.unsplash.com/search/photos?query=${keyword}`);
+  //     let jsonifiedResponse;
+  //     if (response.ok && response.status === 200) {
+  //       jsonifiedResponse = await response.json();
+  //     } else {
+  //       jsonifiedResponse = false;
+  //     }
+  //     let myResult = jsonifiedResponse;
+  //     if (myResult) {
+  //       console.log(myResult);
+  //     }
+  //     else {
+  //       console.log('error this is else');
+  //       // $('#results').append(`Couldn't find a pokemon named '${poke}'. Please check spelling and try again!`);
+  //     }
+        
+  //   } catch(e) {
+  //     alert(e.message);
+  //   }
+  // })();
+
+
+  /* Lets the user press "enter" to press button */
+  $('input').bind('keypress',function(e) {
+    let event = e || window.event;
+    let keycode = event.keyCode || event.which;
+    if(keycode == '13') $(this).siblings("button").click();
   });
 });
-
-
-// const outputDiv = $("div#results");
-// outputDiv.append(`
-//   <h1>Doctors</h1>
-//   <h3>Your primary symptom is ${symptom}. Here's a list of doctors that can help!</h3>
-// `);
